32dd255a60c27d42ad6a0c45d3f47d1d56773631
Fawzya Said <144002368+fawzya195@users.noreply.github.com>
2025-05-06 19:06:00 +0300

    Add files via upload

---
+++ Task-9/VideosFrom-123To-133/main.js
@@ -0,0 +1,63 @@
+
+//Task 2
+// let myFriends = ["Osama", "Ahmed", "Sayed", "Sayed", "Mahmoud", "Osama"];
+// console.log(Array.from(new Set(myFriends)).sort()); 
+
+
+
+
+
+
+//Task 3
+// let myInfo = {
+//     username: "Osama",
+//     role: "Admin",
+//     country: "Egypt",
+//   };
+//   let myMap=new Map(Object.entries(myInfo));
+//   console.log(myMap); 
+//   console.log(myMap.size);
+//   console.log(myMap.has("role"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Task 5
+// let theName = "Elzero";
+// console.log([...theName])  //first way
+
+// console.log(Array.from(theName)) //second way
+
+// console.log(theName.split("")) //third way
+
+// let arr4 = [];
+// for (let i = 0; i < theName.length; i++) {
+//     arr4.push(theName[i]);
+// }console.log(arr4) //fourth way
+
+// let arr5 = theName.split("").map(char => char); 
+// console.log(arr5); //fifth way
+
+
+
+
+
+
+// Task 7
+// let numsOne = [1, 2, 3];
+// let numsTwo = [4, 5, 6];
+
+// console.log(numsOne.concat(numsTwo)); //first way
+// console.log([...numsOne, ...numsTwo]); //second way
+//  console.log(numsOne.concat(numsTwo)); //third way
---
+++ Task-9/VideosFrom134To146/main.js
@@ -0,0 +1,30 @@
+// Task 1
+// let ip = "2001:db8:3333:4444:5555:6666:7777:8888";
+// let ipRe=/\d{4}:\w{2}\d:\d{4}:\d{4}:\d{4}:\d{4}:\d{4}:\d{4}/g;
+// console.log(ip.match(ipRe));
+
+
+
+
+
+
+
+// Task2
+// let specialNames = "Os10O OsO Os100O Osa100O Os1000 Os100m";
+// let specialNamesRe = /\bOs\d*O\b/g;
+// let specialNamesResult = specialNames.match(specialNamesRe);
+// console.log(specialNamesResult);
+
+
+
+
+
+
+
+
+
+// Task3
+// let phone = "+(995)-123 (4567)";
+// let phoneRe = /\+\(\d{3}\)-\d{3} \(\d{4}\)/g;
+// let phoneResult = phone.match(phoneRe); 
+// console.log(phoneResult);
